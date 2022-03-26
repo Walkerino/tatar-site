2afc34fa99d34e94ebb21af479674c4a4d4c78fa
Walkerino <80414290+Walkerino@users.noreply.github.com>
2022-03-26 09:18:50 +0300

    optimizaton main.js

--- js/main.js
+++ js/main.js
@@ -58,14 +58,3 @@ $(document).ready(function() {
     checkButtons();
 });
 
-
-//marqueeline
-// function animate(id) {
-//     var node = document.getElementById(id).childNodes[0];
-//     var text = node.data;
-//     setInterval(function() {
-//         text = text.substring(1) + text[0];
-//         node.data = text;
-//     }, 125);
-// }
-// window.addEventListener('load', function(e) { animate('line'); }, false);
