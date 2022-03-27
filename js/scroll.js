5709a4ff51e8e20e85844c190a0b07b7c317895a
Walkerino <80414290+Walkerino@users.noreply.github.com>
2022-03-27 23:51:04 +0300

    Delete js directory

--- js/scroll.js
+++
@@ -1,7 +0,0 @@
-//animated scroll on JQuery 
-
-$('a[href^="#"]').click(function(){
-    let anchor = $(this).attr('href');
-    $('html, body').animate({
-    scrollTop:  $(anchor).offset().top}, 600);
-});
